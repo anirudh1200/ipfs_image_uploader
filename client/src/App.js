a195f7153396c7c1a5d424e42c4391a62e789bf3
= <anirudh.b.123@gmail.com>
2019-01-02 00:46:37 +0530

    added basic encryption

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import ipfs from "./ipfs";
-
+import crypto from "crypto";
 import "./App.css";
+import axios from 'axios';
 
 class App extends Component {
-  state = { web3: null, account: null, contract: null, buffer: null, ipfsHash: '', status: '' };
+  state = { web3: null, account: null, contract: null, buffer: null, ipfsHash: '', status: '', image: '', password: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', iv: 'aaaaaaaaaaaaaaaa' };
 
   componentDidMount = async () => {
     try {
@@ -48,26 +49,48 @@ class App extends Component {
 
     // Update state with the result.
     this.setState({ ipfsHash: response });
+    console.log(this.state.ipfsHash);
+    fetch(`https://ipfs.io/ipfs/${this.state.ipfsHash}`)
+        .then(res => {
+            console.log(res);
+            res.arrayBuffer()
+                .then(buffer => {
+                    buffer = new Uint8Array(buffer);
+                    let decrypted = this.decrypt(buffer);
+                    console.log('decrypted:',decrypted);
+                    this.setState({ image: decrypted });
+                })
+        });
   };
 
   handleSubmit = async e => {
       e.preventDefault();
-      const { account, contract } = this.state;
       this.setState({ status: 'Uploading to ipfs' });
-      //Upload image to ipfs
-      ipfs.files.add(this.state.buffer, async (error, result) => {
+      console.log(this.state.buffer);
+      let encrypted = this.encrypt(this.state.buffer);
+      this.uploadFile(encrypted);
+      console.log(encrypted);
+      console.log(this.decrypt(encrypted));
+  };
+
+  //Upload image to ipfs
+  uploadFile = (encryptedData) => {
+      const { account, contract } = this.state;
+      ipfs.files.add(encryptedData, async (error, result) => {
           if(error){
               console.error(error);
               return;
           }
           this.setState({ status: 'Uploading to blockchain' });
+          //For debugging
+          console.log(result[0].hash);
           // Stores a given ipfsHash to contract
           await contract.methods.set(result[0].hash).send({ from: account });
           // Stores the ipfs hash to state
           this.setState({ ipfsHash: await contract.methods.get().call() });
           this.setState({ status: 'Transaction Successful' });
       });
-  };
+  }
 
   captureFile = e => {
       e.preventDefault();
@@ -79,19 +102,28 @@ class App extends Component {
       }
   };
 
+  encrypt = (buffer) => {
+    var cipher = crypto.createCipheriv('aes-256-ctr',this.state.password,this.state.iv)
+    var crypted = Buffer.concat([cipher.update(buffer),cipher.final()]);
+    return crypted;
+  }
+
+  decrypt = (buffer) => {
+    var decipher = crypto.createDecipheriv('aes-256-ctr',this.state.password,this.state.iv)
+    var dec = Buffer.concat([decipher.update(buffer) , decipher.final()]);
+    return dec;
+  }
+
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    var url;
-    if(this.state.ipfsHash){
-        url = `https://ipfs.io/ipfs/${this.state.ipfsHash}`;
+    var image;
+    if(this.state.image){
+        image = 'data:image/jpeg;base64,' + this.state.image.toString('base64');
     }
     return (
       <div className="App">
         <h1>Your Image</h1>
         <p>This image is stored on IPFS & the Ethereum Blockchain!</p>
-        <img src={url} alt="Stored on IPFS & the Ethereum Blockchain"/>
+        <img src={image} alt="Stored on IPFS & the Ethereum Blockchain"/>
         <h2>Upload Image</h2>
         <form onSubmit={this.handleSubmit}>
             <input type="file" onChange={this.captureFile} />
