6202473335756704307e7724699f9c055824100c
= <anirudh.b.123@gmail.com>
2019-01-10 00:23:49 +0530

    changed contract structure

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,32 @@
 pragma solidity 0.4.25;
 
 contract SimpleStorage {
-  string ipfsHash;
 
-  function set(string x) public {
-    ipfsHash = x;
-  }
+    struct hash{
+        string ipfsHash;
+        uint timestamp;
+    }
 
-  function get() public view returns (string) {
-    return ipfsHash;
-  }
+    uint public totalHashes;
+    hash[] hashList;
+
+    constructor() public {
+        totalHashes = 0;
+    }
+
+    function addHash(string ipfsUrl) public {
+        hash memory newHash = hash({
+           ipfsHash: ipfsUrl,
+           timestamp: now
+        });
+        hashList.push(newHash);
+        totalHashes++;
+    }
+
+    function getHash(uint index) public view returns(string, uint){
+        return(
+            hashList[index].ipfsHash,
+            hashList[index].timestamp
+        );
+    }
 }
